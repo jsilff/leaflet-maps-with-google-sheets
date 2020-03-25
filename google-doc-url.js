9f48e91872cddc6f7086563ad022438de7bb83c8
Jonathan Silberman <jsil@fearless-future.com>
2020-03-25 16:18:20 -0400

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1t3XxNSYZCvmTjGg-N91LtcITe00XU2DtvgxivTcQYA0/edit#gid=0';
